3f9a774b563a89cab024427931017cafe98e67ec
Asterios <universegaming234@gmail.com>
2026-05-26 09:21:33 +0700

    fix(section): add authenticate to route

--- src/routes/client/section.route.ts
+++ src/routes/client/section.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import * as sectionController from '../../controllers/section.controller.js'
+import { authenticate } from '../../middlewares/auth.middleware.js'
 
 const router = Router()
 
-router.get('/:sectionId', sectionController.getSectionById)
+router.get('/:sectionId', authenticate, sectionController.getSectionById)
 
 export default router
