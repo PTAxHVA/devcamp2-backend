6a7987b0ecd5e89858dab873d942e73ac2c2bc3f
Asterios <universegaming234@gmail.com>
2026-05-26 07:35:42 +0700

    fix(topic): fix topic logic

--- src/services/topic.service.ts
+++ src/services/topic.service.ts
@@ -1,20 +1,46 @@
-import { MasterTopic } from '../models/master-topic.model.js'
 import { Section } from '../models/section.model.js'
 import { ApiError } from '../utils/api-error.js'
 import { UserSectionProgress } from '../models/user-section-progress.model.js'
+import { UserRoadmap } from '../models/user-roadmap.model.js'
+import { UserTopic } from '../models/user-topic.model.js'
+import { isValidObjectId } from 'mongoose'
+import { MasterTopic } from '../models/master-topic.model.js'
 
 export const getTopicById = async (topicId: string, userId: string) => {
-  const topic = await MasterTopic.findById(topicId)
-  const sectionInTopic = await Section.find({ topicId })
-  const userProgress = await UserSectionProgress.find({ topicId, userId })
+  if (!isValidObjectId(topicId)) {
+    throw new ApiError(400, 'Invalid topic id', 'INVALID_TOPIC_ID')
+  }
+
+  const userRoadmap = await UserRoadmap.find({ userId, isActive: true }).select('_id').lean()
+  if (userRoadmap.length === 0) {
+    throw new ApiError(404, 'User roadmap not found', 'USER_ROADMAP_NOT_FOUND')
+  }
+
+  const userTopic = await UserTopic.find({
+    userRoadmapId: { $in: userRoadmap.map((r) => r._id) },
+    topicId,
+  })
+    .select('_id')
+    .lean()
+  if (userTopic.length === 0) {
+    throw new ApiError(404, 'User topic not found', 'USER_TOPIC_NOT_FOUND')
+  }
+
+  const topic = await MasterTopic.findById(topicId).select('_id').lean()
   if (!topic) {
-    throw new ApiError(404, 'Topic not found')
+    throw new ApiError(404, 'Topic not found', 'TOPIC_NOT_FOUND')
   }
-  if (!sectionInTopic) {
-    throw new ApiError(404, 'Sections not found')
+
+  const sections = await Section.find({ topicId, isPublished: true }).sort({ orderIndex: 1 }).lean()
+  if (!sections) {
+    throw new ApiError(404, 'Sections not found', 'SECTIONS_NOT_FOUND')
   }
-  if (!userProgress) {
-    throw new ApiError(404, 'User progress not found')
-  }
-  return { topic, sectionInTopic, userProgress }
+  const sectionsId = sections.map((s) => s._id)
+
+  const userProgress = await UserSectionProgress.find({
+    userTopicId: { $in: userTopic.map((u) => u._id) },
+    sectionId: { $in: sectionsId },
+  }).lean()
+
+  return { userTopic, sections, userProgress }
 }
