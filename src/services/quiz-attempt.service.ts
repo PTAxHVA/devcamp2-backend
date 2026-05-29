933a0904e06e95db346e2a3a55afcafd5c286aa3
Asterios <universegaming234@gmail.com>
2026-05-29 19:08:14 +0700

    fix(quiz-attempt): fix attempt get to take userId

--- src/services/quiz-attempt.service.ts
+++ src/services/quiz-attempt.service.ts
@@ -3,11 +3,11 @@ import { isValidObjectId } from 'mongoose'
 import { QuizAttempt } from '../models/quiz-attempt.model.js'
 import { QuizAttemptAnswer } from '../models/quiz-attempt-answer.model.js'
 
-export const getAttempt = async (attemptId: string) => {
+export const getAttempt = async (attemptId: string, userId: string) => {
   if (!isValidObjectId(attemptId))
     throw new ApiError(400, 'Invalid attempt id', 'INVALID_OBJECT_ID')
 
-  const attempt = await QuizAttempt.findById({ _id: attemptId }, 'userId').lean()
+  const attempt = await QuizAttempt.findOne({ _id: attemptId, userId }).lean()
 
   if (!attempt) throw new ApiError(404, 'Attempt not found', 'ATTEMPT_NOT_FOUND')
 
