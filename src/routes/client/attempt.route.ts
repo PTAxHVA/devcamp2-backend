bd22c3d376ee6ec935d57f5c624a2dbd733ce53a
Asterios <universegaming234@gmail.com>
2026-05-29 20:53:20 +0700

    chore(quiz-attempt): move routes

--- src/routes/client/attempt.route.ts
+++ src/routes/client/attempt.route.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express'
 import { authenticate } from '../../middlewares/auth.middleware.js'
+import { validate } from '../../middlewares/validate.middleware.js'
+import { submitAttemptSchema } from '../../schemas/quiz.schema.js'
 import * as quizAttemptController from '../../controllers/quiz.controller.js'
 
 const router = Router()
 
 router.get('/:id', authenticate, quizAttemptController.getAttempt)
 router.get('/:id/result', authenticate, quizAttemptController.getAttemptResult)
+router.post(
+  '/:id/submit',
+  authenticate,
+  validate(submitAttemptSchema),
+  quizAttemptController.submitAndGradeQuiz,
+)
 
 export default router
