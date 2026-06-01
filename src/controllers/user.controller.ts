c170bccc229b2850c78603d25ab46acd12962e3a
Asterios <universegaming234@gmail.com>
2026-06-01 19:05:38 +0700

    feat(profile): implement get me and profile logic

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,43 @@ import { NextFunction, Request, Response } from 'express'
 import * as streakService from '../services/streak.service.js'
 import * as progressService from '../services/progress.service.js'
 import { ok } from '../utils/api-response.js'
+import * as userService from '../services/user.service.js'
+
+export const getUserDetails = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const userId = req.user?.id as string
+
+    const user = await userService.getUser(userId)
+
+    res.json(ok(user))
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getProfile = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const userId = req.user?.id as string
+
+    const profile = await userService.getProfile(userId)
+
+    res.json(ok(profile))
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const updateProfile = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const userId = req.user?.id as string
+
+    const profile = await userService.updateProfile(userId, req.body)
+
+    res.json(ok(profile))
+  } catch (error) {
+    next(error)
+  }
+}
 
 export const getUserStreak = async (req: Request, res: Response, next: NextFunction) => {
   try {
--- src/routes/client/user.route.ts
+++ src/routes/client/user.route.ts
@@ -4,6 +4,9 @@ import * as userController from '../../controllers/user.controller.js'
 
 const route = Router()
 
+route.get('/', authenticate, userController.getUserDetails)
+route.get('/profile', authenticate, userController.getProfile)
+route.patch('/profile', authenticate, userController.updateProfile)
 route.get('/streak', authenticate, userController.getUserStreak)
 route.get('/progress', authenticate, userController.getUserProgress)
 
