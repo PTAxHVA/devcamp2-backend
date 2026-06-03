882b85316433c696f907d3be9de6f73624ae5316
Asterios <universegaming234@gmail.com>
2026-06-03 18:39:56 +0700

    chore(mongo-idx): index quizAttempt

--- src/models/user-section-progress.model.ts
+++ src/models/user-section-progress.model.ts
@@ -21,7 +21,7 @@ const userSectionProgressSchema = new Schema<IUserSectionProgress>(
   { timestamps: true },
 )
 
-// Unique progress per (userTopic, section).
+// index for fetching and sorting user's section progress
 userSectionProgressSchema.index({ userTopicId: 1, sectionId: 1 }, { unique: true })
 
 export const UserSectionProgress = model<IUserSectionProgress>(
